343bb099ddc7c27fa9ad06d31d00c1edc9eca0bc
manikandanbnair <89865543+manikandanbnair@users.noreply.github.com>
2023-01-19 07:24:34 +0530

    Update navbar.jsx

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -15,10 +15,22 @@ function navbar()
         }
     }
     window.addEventListener('scroll',changeBackground);
-    return{
-        <nav className ={nav ? "nav active" : "nav"}>
-
+    return(
+        <nav className={nav ? "nav active" : "nav"}>
+            <Link to= '#' className='logo'>
+                <img src={logo} alt=''/>
+                </Link>
+                <input className='menu-btn' type ='checkbox' id='menu-btn'/>
+                <label className='menu-icon' for='menu-btn'>
+                    <span className='nav-icon'></span>
+                </label>
+                <ul className="menu">                    <li><Link to='# '>Header</Link></li>
+                    <li><Link to='# '>Header</Link></li>
+                    <li><Link to='# '>Header</Link></li>
+                </ul>
         </nav>
-    }
+
+        
+    )
 }
 export default navbar
